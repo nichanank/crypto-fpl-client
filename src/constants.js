2dcb405f29cbdbe314bb509b099932d420077909
nichanank <n.kesonpat@gmail.com>
2019-07-22 21:57:47 +0700

    updated teams for 2019/2020 season

--- src/constants.js
+++ src/constants.js
@@ -11,20 +11,20 @@ export function position(x) {
 export function team(x) {
   switch(x) {
     case 3: return 'Arsenal';
+    case 7: return 'Aston Villa';
     case 91: return 'Bournemouth';
     case 36: return 'Brighton';
     case 90: return 'Burnley';
-    case 97: return 'Cardiff';
     case 8: return 'Chelsea';
     case 31: return 'Crystal Palace';
     case 11: return 'Everton';
-    case 54: return 'Fulham';
-    case 38: return 'Huddersfield';
     case 13: return 'Leicester';
     case 14: return 'Liverpool';
     case 43: return 'Man City';
     case 1: return 'Man United';
     case 4: return 'Newcastle';
+    case 45: return 'Norwich';
+    case 49: return 'Sheffield Utd';
     case 20: return 'Southampton';
     case 6: return 'Spurs';
     case 57: return 'Watford';
